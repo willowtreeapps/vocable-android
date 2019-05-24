314e43ad5b38ab2f731003c5a245ae53a1ee13d4
Vaan Waber <vwaber@gmail.com>
2019-05-24 14:15:17 -0400

    Update PointerView

--- app/src/main/java/com/example/eyespeak/PointerView.kt
+++ app/src/main/java/com/example/eyespeak/PointerView.kt
@@ -5,16 +5,39 @@ import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.RectF
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 
 class PointerView(context: Context, attrs: AttributeSet) : View(context, attrs){
 
     private val paint: Paint = Paint()
-    val rectF: RectF = RectF(100.0F, 100.0F, 200.0F, 200.0F)
+    private val radius: Float = 100f
+
+    private var xAdjusted: Float = 0f
+    private var yAdjusted: Float = 0f
+
+    private var xPercent: Int = 0
+    private var yPercent: Int = 0
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        canvas?.drawOval(rectF, paint)
+        canvas?.drawCircle(xAdjusted, yAdjusted, radius, paint)
+    }
+
+    fun updatePointerPosition(xPercent: Int, yPercent: Int){
+        this.xPercent = xPercent
+        this.yPercent = yPercent
+        invalidate()
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        calcAdjustedCoords(w, h)
+    }
+
+    private fun calcAdjustedCoords(width: Int, height: Int){
+        xAdjusted = (width/100F * xPercent)
+        yAdjusted = (height/100F * yPercent)
     }
 
 }
