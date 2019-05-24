61cf628e307fe31b3174c51470fed88641d05ac1
Vaan Waber <vwaber@gmail.com>
2019-05-24 14:15:17 -0400

    Update PointerView

--- app/src/main/java/com/example/eyespeak/MainActivity.kt
+++ app/src/main/java/com/example/eyespeak/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.eyespeak
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        pointer_view.updatePointerPosition(100, 100)
     }
 }
