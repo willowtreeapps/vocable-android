9dd0c4e7a726a49e11199636b881948b78bb99a2
Nick Cook <nick.cook@willowtreeapps.com>
2020-02-14 10:05:14 -0500

    Add keyboard fragment

--- app/src/main/java/com/willowtree/vocable/MainActivity.kt
+++ app/src/main/java/com/willowtree/vocable/MainActivity.kt
@@ -3,13 +3,17 @@ package com.willowtree.vocable
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.LayoutRes
 import androidx.core.view.children
+import com.willowtree.vocable.customviews.ActionButton
 import com.willowtree.vocable.customviews.PauseButton
 import com.willowtree.vocable.customviews.PointerListener
 import com.willowtree.vocable.customviews.PointerView
+import com.willowtree.vocable.keyboard.KeyboardFragment
 import com.willowtree.vocable.presets.PresetsFragment
 import com.willowtree.vocable.utils.VocableTextToSpeech
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.presets_action_buttons.*
 
 
 class MainActivity : BaseActivity() {
@@ -23,6 +27,10 @@ class MainActivity : BaseActivity() {
             .beginTransaction()
             .replace(R.id.fragment_container, PresetsFragment())
             .commit()
+
+        keyboard_button.action = {
+            loadFragment(KeyboardFragment())
+        }
     }
 
     override fun onDestroy() {
@@ -60,6 +68,42 @@ class MainActivity : BaseActivity() {
         supportFragmentManager.fragments.forEach {
             if (it is BaseFragment) {
                 allViews.addAll(it.getAllViews())
+            }
+        }
+    }
+
+    private fun loadFragment(fragment: BaseFragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+        allViews.clear()
+
+        when (fragment) {
+            is PresetsFragment -> {
+                setUpActionButtons(R.layout.presets_action_buttons)
+            }
+            is KeyboardFragment -> {
+                setUpActionButtons(R.layout.keyboard_action_buttons)
+            }
+        }
+    }
+
+    private fun setUpActionButtons(@LayoutRes buttonLayout: Int) {
+        with(action_button_container) {
+            removeAllViews()
+            layoutInflater.inflate(buttonLayout, action_button_container, true)
+        }
+        when (buttonLayout) {
+            R.layout.presets_action_buttons -> {
+                findViewById<ActionButton>(R.id.keyboard_button).action = {
+                    loadFragment(KeyboardFragment())
+                }
+            }
+            R.layout.keyboard_action_buttons -> {
+                findViewById<ActionButton>(R.id.presets_button).action = {
+                    loadFragment(PresetsFragment())
+                }
             }
         }
     }
