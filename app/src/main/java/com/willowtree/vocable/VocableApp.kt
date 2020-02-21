8f8a692246704a1b36641903ccd479f041fd49ad
joshuaqueen <joshua.queen@willowtreeapps.com>
2020-02-21 13:36:21 -0500

    adding text builder for material icons

--- app/src/main/java/com/willowtree/vocable/VocableApp.kt
+++ app/src/main/java/com/willowtree/vocable/VocableApp.kt
@@ -1,13 +1,38 @@
 package com.willowtree.vocable
 
 import android.app.Application
+import android.content.Context
+import android.content.res.AssetManager
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.view.View
+import android.widget.TextView
+import io.github.inflationx.calligraphy3.CalligraphyConfig
+import io.github.inflationx.calligraphy3.CalligraphyInterceptor
+import io.github.inflationx.calligraphy3.CalligraphyTypefaceSpan
+import io.github.inflationx.calligraphy3.TypefaceUtils
+import io.github.inflationx.viewpump.ViewPump
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
+
 
 class VocableApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        ViewPump.init(
+            ViewPump.builder()
+                .addInterceptor(
+                    CalligraphyInterceptor(
+                        CalligraphyConfig.Builder()
+                            .setDefaultFontPath("fonts/Roboto-RobotoRegular.ttf")
+                            .setFontAttrId(R.attr.fontPath)
+                            .build()
+                    )
+                )
+                .build()
+        )
 
         startKoin {
             androidContext(this@VocableApp)
