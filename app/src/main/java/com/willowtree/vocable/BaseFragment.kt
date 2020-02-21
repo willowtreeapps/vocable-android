e9d5c270610ca8339db3fd063e02b738f46eb568
joshuaqueen <joshua.queen@willowtreeapps.com>
2020-02-21 16:10:38 -0500

    pull the icon text builder into the base fragment and set it to use imageSpan instead of custom font

--- app/src/main/java/com/willowtree/vocable/BaseFragment.kt
+++ app/src/main/java/com/willowtree/vocable/BaseFragment.kt
@@ -1,11 +1,19 @@
 package com.willowtree.vocable
 
+import android.content.res.Resources
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.DynamicDrawableSpan
+import android.text.style.ImageSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.phrase_saved_success.*
 
 abstract class BaseFragment : Fragment() {
 
@@ -21,4 +29,20 @@ abstract class BaseFragment : Fragment() {
     protected abstract fun getLayout(): Int
 
     abstract fun getAllViews(): List<View>
+
+    protected fun buildTextWithIcon(vararg strings: String, iconCharStart: Int, iconCharEnd: Int, view: TextView, icon: Int ) {
+        val sBuilder = SpannableStringBuilder()
+
+        for (item in strings) {
+            sBuilder.append(item)
+        }
+        val imageSpan = ImageSpan(
+            requireContext(),
+            icon,
+            DynamicDrawableSpan.ALIGN_BASELINE
+        )
+        sBuilder.setSpan(imageSpan, iconCharStart, iconCharEnd, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        view.setText(sBuilder, TextView.BufferType.SPANNABLE)
+    }
 }
