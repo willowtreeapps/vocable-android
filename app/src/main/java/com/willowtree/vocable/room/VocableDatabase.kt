fa2954da9152d5bd9c20ee4007b873514bf25ec4
Caroline Law <caroline.law@willowtreeapps.com>
2020-03-19 14:42:31 -0400

    fix crash, migrate database

--- app/src/main/java/com/willowtree/vocable/room/VocableDatabase.kt
+++ app/src/main/java/com/willowtree/vocable/room/VocableDatabase.kt
@@ -4,17 +4,26 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 
-@Database(entities = [Category::class, Phrase::class], version = 1)
+@Database(entities = [Category::class, Phrase::class], version = 2)
 abstract class VocableDatabase : RoomDatabase() {
 
     companion object {
         private var vocableDatabase: VocableDatabase? = null
 
+        private val MIGRATION_1_2: Migration = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("DROP INDEX index_Phrase_utterance")
+            }
+        }
+
         fun getVocableDatabase(context: Context): VocableDatabase {
             if (vocableDatabase == null) {
                 vocableDatabase =
                     Room.databaseBuilder(context, VocableDatabase::class.java, "VocableDatabase")
+                        .addMigrations(MIGRATION_1_2)
                         .build()
             }
             return vocableDatabase as VocableDatabase
@@ -25,3 +34,4 @@ abstract class VocableDatabase : RoomDatabase() {
 
     abstract fun phraseDao(): PhraseDao
 }
+
