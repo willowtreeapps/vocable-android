f31288a72f1aec5309fd70f6e115a47bcf1e798d
Nick Cook <nick.cook@willowtreeapps.com>
2020-04-19 15:16:17 -0400

    Make scrolling smoother

--- app/src/main/java/com/willowtree/vocable/utils/VocableFragmentStateAdapter.kt
+++ app/src/main/java/com/willowtree/vocable/utils/VocableFragmentStateAdapter.kt
@@ -6,29 +6,37 @@ import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import kotlin.math.ceil
 
+/**
+ * A custom implementation of FragmentStateAdapter to solve an issue with fragments not being
+ * properly updated when new data was added. If the default implementations of getItemId() and
+ * containsItem() are left unchanged, the adapter will not update any previously created fragments
+ * with new data. This custom class solves this issue by giving unique ids to each fragment and
+ * updating the ids every time the data set is changed with setItems().
+ */
 abstract class VocableFragmentStateAdapter(fm: FragmentManager, lifecycle: Lifecycle) :
     FragmentStateAdapter(fm, lifecycle) {
 
     private val items = mutableListOf<Any>()
-    private var pageIds = listOf<Long>()
-    private val emptyId = System.currentTimeMillis()
+    private var baseId = 0L
     var numPages = 0
 
-    override fun getItemCount(): Int = Int.MAX_VALUE
+    override fun getItemCount(): Int {
+        return if (items.isEmpty() || getMaxItemsPerPage() >= items.size) {
+            1
+        } else {
+            Int.MAX_VALUE
+        }
+    }
 
     @CallSuper
     open fun setItems(items: List<Any>) {
+        baseId = System.currentTimeMillis()
         with(this.items) {
             clear()
             addAll(items)
-            pageIds = if (items.isEmpty()) {
-                listOf(emptyId)
-            } else {
-                map { it.hashCode().toLong() }
-            }
         }
 
-        numPages = if (items.isEmpty()) {
+        numPages = if (items.isEmpty() || getMaxItemsPerPage() >= items.size) {
             1
         } else {
             ceil(items.size / getMaxItemsPerPage().toDouble()).toInt()
@@ -38,14 +46,12 @@ abstract class VocableFragmentStateAdapter(fm: FragmentManager, lifecycle: Lifec
     }
 
     override fun getItemId(position: Int): Long {
-        if (items.isEmpty()) {
-            return emptyId
-        }
-        return pageIds[position % numPages]
+        return baseId - position
     }
 
     override fun containsItem(itemId: Long): Boolean {
-        return pageIds.contains(itemId)
+        val position = baseId - itemId
+        return position in 0 until itemCount
     }
 
     abstract fun getMaxItemsPerPage(): Int
