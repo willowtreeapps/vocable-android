118320e60fb9d351e2f17c2c41ea2b04383c9b4a
joshuaqueen <joshua.queen@willowtreeapps.com>
2020-04-20 15:20:52 -0400

    adding espresso framework, a test, and updating circle config

---
+++ app/src/androidTest/java/com/willowtree/vocable/pages/PresetsPage.kt
@@ -0,0 +1,35 @@
+package com.willowtree.vocable.pages
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.matcher.ViewMatchers.*
+import com.willowtree.vocable.R
+import com.willowtree.vocable.customviews.VocableButton
+import org.hamcrest.CoreMatchers.allOf
+import org.hamcrest.CoreMatchers.instanceOf
+
+class PresetsPage {
+
+    // Edit Text box and action buttons
+    val currentText = onView(withId(R.id.current_text))
+    val keyboardNavitgationButton = onView(withId(R.id.keyboard_button))
+    val settingsNavigationButton = onView(withId(R.id.settings_button))
+
+    // Categories and preset phrases
+    val categoryBackButton = onView(withId(R.id.category_back_button))
+    val categoryForwardButton = onView(withId(R.id.category_forward_button))
+
+    // First phrase of selected category
+    val firstPhrase = onView(
+            allOf(
+                    instanceOf(VocableButton::class.java),
+                    withParent(withId(R.id.phrases_container)),
+                    withParentIndex(0)
+            )
+    )
+
+    // These will appear on other pages and could be abstracted out eventually
+    val phrasesBackButton = onView(withId(R.id.phrases_back_button))
+    val phrasesForwardButton = onView(withId(R.id.phrases_forward_button))
+    val phrasesPageNumber = onView(withId(R.id.phrases_page_number))
+
+}
---
+++ app/src/androidTest/java/com/willowtree/vocable/tests/PresetsPageTest.kt
@@ -0,0 +1,37 @@
+package com.willowtree.vocable.tests
+
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import com.willowtree.vocable.MainActivity
+import com.willowtree.vocable.pages.PresetsPage
+import com.willowtree.vocable.splash.SplashActivity
+import com.willowtree.vocable.utility.BaseUITest
+import com.willowtree.vocable.utility.assertTextMatches
+import com.willowtree.vocable.utility.tap
+
+import org.junit.Test
+
+class PresetsPageTest : BaseUITest<SplashActivity>(){
+
+    private val presetsPage = PresetsPage()
+
+    override fun getActivityTestRule(): IntentsTestRule<SplashActivity> {
+        return IntentsTestRule(
+                SplashActivity::class.java, false, false
+        )
+    }
+
+    // Eventually we may want to get the currently selected category and then pull the first phrase of that category from the JSON
+    // to make this test more dynamic.
+    @Test
+    fun verifyClickingPhraseUpdatesCurrentText() {
+
+        // Wait for Splash Screen
+        Thread.sleep(3000)
+
+        presetsPage.apply {
+            firstPhrase.tap()
+            currentText.assertTextMatches("Please")
+        }
+    }
+
+}
