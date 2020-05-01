f353ec1341dca1e6f60ccfbcfeb3a9fb3ba77ca3
Joseph Wysosky <wysoskyj@gmail.com>
2020-05-01 12:13:36 -0400

    Current version of idling resource to wait for the main screen to be available

--- app/src/androidTest/java/com/willowtree/vocable/tests/BaseTest.kt
+++ app/src/androidTest/java/com/willowtree/vocable/tests/BaseTest.kt
@@ -2,17 +2,12 @@ package com.willowtree.vocable.tests
 
 import android.app.Activity
 import android.content.Intent
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
-import androidx.test.uiautomator.UiDevice
-import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Rule
 
 
 abstract class BaseTest<T : Activity> {
-
-    private val device: UiDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
 
     @Rule
     lateinit var activityRule: ActivityTestRule<T>
@@ -20,19 +15,8 @@ abstract class BaseTest<T : Activity> {
     @Before
     fun setup() {
         println("setup")
-        activityRule = getActivityTestRule()
-
-        if (shouldAutoLaunchActivity()) {
-            activityRule.launchActivity(Intent())
-        }
-    }
-
-    protected fun launchActivity() {
-        runBlocking {
-            activityRule.launchActivity(Intent())
-        }
+        getActivityTestRule().launchActivity(Intent())
     }
 
     abstract fun getActivityTestRule(): ActivityTestRule<T>
-    protected open fun shouldAutoLaunchActivity(): Boolean = true
 }
--- app/src/androidTest/java/com/willowtree/vocable/tests/MainScreenTest.kt
+++ app/src/androidTest/java/com/willowtree/vocable/tests/MainScreenTest.kt
@@ -1,9 +1,13 @@
 package com.willowtree.vocable.tests
 
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
+import com.willowtree.vocable.R
 import com.willowtree.vocable.screens.MainScreen
 import com.willowtree.vocable.splash.SplashActivity
+import com.willowtree.vocable.utility.SplashScreenIdlingResource
 import com.willowtree.vocable.utility.assertTextMatches
 import com.willowtree.vocable.utility.tap
 
@@ -17,16 +21,23 @@ class MainScreenTest : BaseTest<SplashActivity>() {
 
     override fun getActivityTestRule(): ActivityTestRule<SplashActivity> {
         return ActivityTestRule(
-            SplashActivity::class.java, false, false
+            SplashActivity::class.java, false, true
         )
     }
 
     @Test
     fun verifyClickingPhraseUpdatesCurrentText() {
+        val idlingResource = SplashScreenIdlingResource(
+            ViewMatchers.withId(R.id.current_text),
+            ViewMatchers.isDisplayed()
+        )
+        val registry = IdlingRegistry.getInstance()
+
+        registry.register(idlingResource)
+
         mainScreen.apply {
             firstPhrase.tap()
             currentText.assertTextMatches("Please")
         }
     }
-
 }
