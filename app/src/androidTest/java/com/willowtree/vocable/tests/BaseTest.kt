b7fc302ef12b5ef2247d8024692c4be157246930
Joseph Wysosky <wysoskyj@gmail.com>
2020-05-01 13:39:00 -0400

    Move the idle registry into the basetest so that it gets setup correctly for all future tests

--- app/src/androidTest/java/com/willowtree/vocable/tests/BaseTest.kt
+++ app/src/androidTest/java/com/willowtree/vocable/tests/BaseTest.kt
@@ -2,20 +2,36 @@ package com.willowtree.vocable.tests
 
 import android.app.Activity
 import android.content.Intent
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.rule.ActivityTestRule
+import com.willowtree.vocable.R
+import com.willowtree.vocable.utility.SplashScreenIdlingResource
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 
 
 abstract class BaseTest<T : Activity> {
 
+    private val idleRegistry = IdlingRegistry.getInstance()
+    private val idlingResource = SplashScreenIdlingResource(
+        ViewMatchers.withId(R.id.current_text),
+        ViewMatchers.isDisplayed()
+    )
+
     @Rule
     lateinit var activityRule: ActivityTestRule<T>
 
     @Before
     fun setup() {
-        println("setup")
+        idleRegistry.register(idlingResource)
         getActivityTestRule().launchActivity(Intent())
+    }
+
+    @After
+    fun teardown() {
+        idleRegistry.unregister(idlingResource)
     }
 
     abstract fun getActivityTestRule(): ActivityTestRule<T>
