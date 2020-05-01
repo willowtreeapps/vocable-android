623d186a1a569d9f86a2015fc2dc1819a7b030d0
Joseph Wysosky <wysoskyj@gmail.com>
2020-05-01 13:39:00 -0400

    Move the idle registry into the basetest so that it gets setup correctly for all future tests

--- app/src/androidTest/java/com/willowtree/vocable/tests/MainScreenTest.kt
+++ app/src/androidTest/java/com/willowtree/vocable/tests/MainScreenTest.kt
@@ -1,13 +1,9 @@
 package com.willowtree.vocable.tests
 
-import androidx.test.espresso.IdlingRegistry
-import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
-import com.willowtree.vocable.R
 import com.willowtree.vocable.screens.MainScreen
 import com.willowtree.vocable.splash.SplashActivity
-import com.willowtree.vocable.utility.SplashScreenIdlingResource
 import com.willowtree.vocable.utility.assertTextMatches
 import com.willowtree.vocable.utility.tap
 
@@ -27,14 +23,6 @@ class MainScreenTest : BaseTest<SplashActivity>() {
 
     @Test
     fun verifyClickingPhraseUpdatesCurrentText() {
-        val idlingResource = SplashScreenIdlingResource(
-            ViewMatchers.withId(R.id.current_text),
-            ViewMatchers.isDisplayed()
-        )
-        val registry = IdlingRegistry.getInstance()
-
-        registry.register(idlingResource)
-
         mainScreen.apply {
             firstPhrase.tap()
             currentText.assertTextMatches("Please")
