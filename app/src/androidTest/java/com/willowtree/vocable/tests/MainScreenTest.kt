f1c46d3c29c3a5381ff18e8d51564018ac2f038b
Joseph Wysosky <wysoskyj@gmail.com>
2020-05-04 19:51:25 -0400

    Rework how we are handling the prompt into a try catch block that shouldn't impact test performance by waiting for it

--- app/src/androidTest/java/com/willowtree/vocable/tests/MainScreenTest.kt
+++ app/src/androidTest/java/com/willowtree/vocable/tests/MainScreenTest.kt
@@ -1,25 +1,16 @@
 package com.willowtree.vocable.tests
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.rule.ActivityTestRule
 import com.willowtree.vocable.screens.MainScreen
-import com.willowtree.vocable.splash.SplashActivity
 import com.willowtree.vocable.utility.assertTextMatches
 import com.willowtree.vocable.utility.tap
-
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class MainScreenTest : BaseTest<SplashActivity>() {
+class MainScreenTest : BaseTest() {
 
     private val mainScreen = MainScreen()
-
-    override fun getActivityTestRule(): ActivityTestRule<SplashActivity> {
-        return ActivityTestRule(
-            SplashActivity::class.java, false, true
-        )
-    }
 
     @Test
     fun verifyClickingPhraseUpdatesCurrentText() {
