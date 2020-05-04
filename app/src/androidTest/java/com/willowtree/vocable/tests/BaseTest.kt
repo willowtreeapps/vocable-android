16094b008e740538e834547c6e8cddcdfeab71aa
Joseph Wysosky <wysoskyj@gmail.com>
2020-05-04 15:16:58 -0400

    Attempt to grab screenshot to see what state the emulator is in on CircleCI

--- app/src/androidTest/java/com/willowtree/vocable/tests/BaseTest.kt
+++ app/src/androidTest/java/com/willowtree/vocable/tests/BaseTest.kt
@@ -2,9 +2,11 @@ package com.willowtree.vocable.tests
 
 import android.app.Activity
 import android.content.Intent
+import android.os.Environment
 import android.util.Log
 import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import androidx.test.rule.ActivityTestRule
 import androidx.test.uiautomator.By
@@ -15,6 +17,7 @@ import com.willowtree.vocable.utility.SplashScreenIdlingResource
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
+import java.io.File
 
 
 abstract class BaseTest<T : Activity> {
@@ -36,7 +39,10 @@ abstract class BaseTest<T : Activity> {
         try {
             device.wait(Until.findObject(By.text("GOT IT")), 30000).click()
         } catch (e: NullPointerException) {
-            Log.e("TEST", "Did not find Got it prompt")
+            val file = File(InstrumentationRegistry.getInstrumentation().targetContext.filesDir.path,
+                "test.png"
+            )
+            device.takeScreenshot(file)
         }
 
     }
