707cdda72ea25f515be8511ddfef5832646e012b
Joseph Wysosky <wysoskyj@gmail.com>
2020-05-04 17:08:53 -0400

    Remove debugging commands and pull out take screenshot functionality for future use

--- app/src/androidTest/java/com/willowtree/vocable/tests/BaseTest.kt
+++ app/src/androidTest/java/com/willowtree/vocable/tests/BaseTest.kt
@@ -2,11 +2,9 @@ package com.willowtree.vocable.tests
 
 import android.app.Activity
 import android.content.Intent
-import android.os.Environment
 import android.util.Log
 import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.platform.app.InstrumentationRegistry.getInstrumentation
 import androidx.test.rule.ActivityTestRule
 import androidx.test.uiautomator.By
@@ -37,18 +35,21 @@ abstract class BaseTest<T : Activity> {
         getActivityTestRule().launchActivity(Intent())
         val device = UiDevice.getInstance(getInstrumentation())
         try {
-            device.wait(Until.findObject(By.text("GOT IT")), 30000).click()
+            device.wait(Until.findObject(By.text("Got it")), 30000).click()
         } catch (e: NullPointerException) {
-            val file = File(getInstrumentation().targetContext.filesDir.path, "test.png")
-            device.takeScreenshot(file)
-            getInstrumentation().uiAutomation.executeShellCommand("run-as com.willowtree.vocable cp $file /sdcard/test.png")
+            Log.d("Test", "Prompt not found, continuing with test")
         }
-
     }
 
     @After
     fun teardown() {
         idleRegistry.unregister(idlingResource)
+    }
+
+    fun takeScreenshot(fileName: String) {
+        val file = File(getInstrumentation().targetContext.filesDir.path, "$fileName.png")
+        UiDevice.getInstance(getInstrumentation()).takeScreenshot(file)
+        getInstrumentation().uiAutomation.executeShellCommand("run-as com.willowtree.vocable cp $file /sdcard/test.png")
     }
 
     abstract fun getActivityTestRule(): ActivityTestRule<T>
