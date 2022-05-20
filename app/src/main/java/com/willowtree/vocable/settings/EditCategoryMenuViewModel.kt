555b06a5ca62b62684a7327d4fe2b4d3bb7c0199
Caroline Wang <cxw320@case.edu>
2022-05-20 11:01:08 -0400

    Add remove category functionality

--- app/src/main/java/com/willowtree/vocable/settings/EditCategoryMenuViewModel.kt
+++ app/src/main/java/com/willowtree/vocable/settings/EditCategoryMenuViewModel.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.willowtree.vocable.BaseViewModel
+import com.willowtree.vocable.presets.PresetCategories
 import com.willowtree.vocable.presets.PresetsRepository
 import com.willowtree.vocable.room.Category
 import com.willowtree.vocable.utils.LocalizedResourceUtility
@@ -28,33 +29,69 @@ class EditCategoryMenuViewModel: BaseViewModel() {
     fun updateCategoryById(categoryId: String) {
         backgroundScope.launch{
             _currentCategory.postValue(presetsRepository.getCategoryById(categoryId))
-            if(_currentCategory.value?.hidden==null){
-                _showCategoryStatus.postValue(true)
-            }else{
-                _showCategoryStatus.postValue(!_currentCategory.value?.hidden!!)
-            }
         }
+    }
 
+    fun retrieveShowStatus(){
+        if(_currentCategory.value?.hidden==null){
+            _showCategoryStatus.postValue(true)
+        }else{
+            _showCategoryStatus.postValue(!_currentCategory.value?.hidden!!)
+        }
     }
 
     fun updateHiddenStatus(showCategoryStatus: Boolean) {
         backgroundScope.launch {
             _currentCategory.value?.hidden = !showCategoryStatus
             _currentCategory?.value?.let { presetsRepository.updateCategory(_currentCategory.value!!) }
-            _currentCategory.value?.categoryId?.let { updateCategoryById(it) }
+            retrieveShowStatus()
         }
     }
 
-    fun updateCurrentCategory(category:Category){
-        _currentCategory.value = category
-    }
-    fun getUpdatedCategoryName(category: Category): String {
-        return localizedResourceUtility.getTextFromCategory(category)
-    }
 
-    fun updateNewCategoryFromRepository(){
-        backgroundScope.launch{
-            _currentCategory.value?.let { presetsRepository.getCategoryById(it.categoryId) }
+    fun deleteCategory(){
+        backgroundScope.launch {
+            val category = _currentCategory.value
+
+            // Delete any phrases whose only associated category is the one being deleted
+            // First get the ids of all phrases associated with the category being deleted
+            val phrasesForCategory = category?.let {
+                presetsRepository.getPhrasesForCategory(it.categoryId)
+                    .sortedBy { it.sortOrder }
+            }
+
+
+            //Delete from Recents by utterance
+            if (phrasesForCategory != null) {
+                presetsRepository.deletePhrases(
+                    presetsRepository.getPhrasesForCategory(PresetCategories.RECENTS.id)
+                        .filter {
+                            phrasesForCategory.map { phrase ->
+                                phrase.localizedUtterance
+                            }.contains(it.localizedUtterance)
+                        }
+                )
+            }
+
+
+            //Delete phrases
+            presetsRepository.deletePhrases(
+                phrasesForCategory?:listOf()
+            )
+
+            // Delete the category
+            if (category != null) {
+                presetsRepository.deleteCategory(category)
+            }
+
+            // Update the sort order of remaining categories
+            val categoriesToUpdate = overallCategories.filter { it.sortOrder > category?.sortOrder!! }
+            categoriesToUpdate.forEach {
+                it.sortOrder--
+            }
+            if (categoriesToUpdate.isNotEmpty()) {
+                presetsRepository.updateCategories(categoriesToUpdate)
+            }
         }
     }
 
