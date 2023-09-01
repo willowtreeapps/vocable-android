b804a73b710768a13c7d4403086858b22685b3fa
Paul Klauser <paul.klauser@willowtreeapps.com>
2023-09-01 16:53:43 -0400

    Add PresetsViewModelTests
    Lot of dependencies needed to come in to effectively test both coroutines and LiveData.

--- app/src/main/java/com/willowtree/vocable/CategoriesUseCase.kt
+++ app/src/main/java/com/willowtree/vocable/CategoriesUseCase.kt
@@ -1,11 +1,11 @@
 package com.willowtree.vocable
 
-import com.willowtree.vocable.presets.PresetsRepository
+import com.willowtree.vocable.presets.IPresetsRepository
 import com.willowtree.vocable.room.CategoryDto
 import kotlinx.coroutines.flow.Flow
 
 class CategoriesUseCase(
-    private val presetsRepository: PresetsRepository
+    private val presetsRepository: IPresetsRepository
 ) {
 
     fun categories(): Flow<List<CategoryDto>> {
--- app/src/main/java/com/willowtree/vocable/presets/IPresetsRepository.kt
+++ app/src/main/java/com/willowtree/vocable/presets/IPresetsRepository.kt
@@ -1,9 +1,12 @@
 package com.willowtree.vocable.presets
 
+import com.willowtree.vocable.room.CategoryDto
 import com.willowtree.vocable.room.Phrase
+import kotlinx.coroutines.flow.Flow
 
 //TODO: PK - Rename this once we make the jump to rename [PresetsRepository] -> "RoomPresetsRepository"
 interface IPresetsRepository {
     suspend fun getPhrasesForCategory(categoryId: String): List<Phrase>
     suspend fun addPhraseToRecents(phrase: Phrase)
+    fun getAllCategoriesFlow(): Flow<List<CategoryDto>>
 }
