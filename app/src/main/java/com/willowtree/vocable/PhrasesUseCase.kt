f06de2dce74136e65e87c974bc99d4d9ee8d5d1f
Paul Klauser <paul.klauser@willowtreeapps.com>
2023-10-04 15:45:18 -0400

    Refactor PhraseUseCase to rely on Phrase instead of PhraseDto

--- app/src/main/java/com/willowtree/vocable/PhrasesUseCase.kt
+++ app/src/main/java/com/willowtree/vocable/PhrasesUseCase.kt
@@ -1,19 +1,20 @@
 package com.willowtree.vocable
 
 import com.willowtree.vocable.presets.IPresetsRepository
+import com.willowtree.vocable.presets.Phrase
 import com.willowtree.vocable.presets.PresetCategories
-import com.willowtree.vocable.room.PhraseDto
+import com.willowtree.vocable.presets.asPhrase
 import com.willowtree.vocable.utils.DateProvider
 
 class PhrasesUseCase(
     private val presetsRepository: IPresetsRepository,
     private val dateProvider: DateProvider
 ) {
-    suspend fun getPhrasesForCategory(categoryId: String): List<PhraseDto> {
+    suspend fun getPhrasesForCategory(categoryId: String): List<Phrase> {
         if (categoryId == PresetCategories.RECENTS.id) {
-            return presetsRepository.getRecentPhrases()
+            return presetsRepository.getRecentPhrases().map { it.asPhrase() }
         }
-        return presetsRepository.getPhrasesForCategory(categoryId)
+        return presetsRepository.getPhrasesForCategory(categoryId).map { it.asPhrase() }
     }
 
     suspend fun phraseSpoken(phraseId: Long) {
