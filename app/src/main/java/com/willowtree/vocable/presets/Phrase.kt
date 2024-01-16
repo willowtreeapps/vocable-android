e7f8fdfa3694cf864c5bda8010318df9e4b76963
Matt Vaughn <matt.vaughn@willowtreeapps.com>
2024-01-16 14:08:24 -0500

    Allow Phrase Text to Come from StringRes
    This is part one of a strategy to make preset phrases all depend on resource IDs instead of strings, for easier runtime translation.
    
    This commit adds in the infrastructure to the view/domain layer to be able to use resource IDs for phrases, but doesn't add any such phrases to the DB. This will be done in a later commit, when database migration/population are handled.

--- app/src/main/java/com/willowtree/vocable/presets/Phrase.kt
+++ app/src/main/java/com/willowtree/vocable/presets/Phrase.kt
@@ -3,11 +3,13 @@ package com.willowtree.vocable.presets
 import android.os.Parcelable
 import com.willowtree.vocable.room.PhraseDto
 import com.willowtree.vocable.utils.locale.LocalesWithText
+import kotlinx.parcelize.IgnoredOnParcel
 import kotlinx.parcelize.Parcelize
 
 sealed class Phrase : Parcelable {
     abstract val phraseId: Long
     abstract val localizedUtterance: LocalesWithText?
+    abstract val utteranceStringRes: Int?
     abstract val sortOrder: Int
 }
 
@@ -15,12 +17,36 @@ sealed class Phrase : Parcelable {
 data class CustomPhrase(
     override val phraseId: Long,
     override val localizedUtterance: LocalesWithText?,
-    override val sortOrder: Int
-) : Phrase(), Parcelable
+    override val sortOrder: Int,
+) : Phrase(), Parcelable {
+    @IgnoredOnParcel
+    override val utteranceStringRes: Int? = null
+}
 
-fun PhraseDto.asPhrase(): Phrase =
-    CustomPhrase(
-        phraseId,
-        localizedUtterance,
-        sortOrder
-    )
+@Parcelize
+data class PresetPhrase(
+    override val phraseId: Long,
+    override val sortOrder: Int,
+    override val utteranceStringRes: Int?,
+) : Phrase() {
+    @IgnoredOnParcel
+    override val localizedUtterance: LocalesWithText? = null
+}
+
+fun PhraseDto.asPhrase(): Phrase {
+    return if (utteranceStringRes != null) {
+        PresetPhrase(
+            phraseId = phraseId,
+            sortOrder = sortOrder,
+            utteranceStringRes = utteranceStringRes
+        )
+    } else if (localizedUtterance != null) {
+        CustomPhrase(
+            phraseId = phraseId,
+            localizedUtterance = localizedUtterance,
+            sortOrder = sortOrder
+        )
+    } else {
+        throw IllegalStateException("Phrase must have either localizedUtterance or utteranceStringRes")
+    }
+}
--- app/src/main/java/com/willowtree/vocable/room/PhraseDto.kt
+++ app/src/main/java/com/willowtree/vocable/room/PhraseDto.kt
@@ -3,6 +3,7 @@ package com.willowtree.vocable.room
 import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Ignore
 import androidx.room.PrimaryKey
 import com.willowtree.vocable.utils.locale.LocalesWithText
 import kotlinx.parcelize.Parcelize
@@ -14,6 +15,28 @@ data class PhraseDto(
     @ColumnInfo(name = "parent_category_id") val parentCategoryId: String?,
     @ColumnInfo(name = "creation_date") val creationDate: Long,
     @ColumnInfo(name = "last_spoken_date") val lastSpokenDate: Long?,
-    @ColumnInfo(name = "localized_utterance") var localizedUtterance: LocalesWithText?,
-    @ColumnInfo(name = "sort_order") var sortOrder: Int
-) : Parcelable
+    @ColumnInfo(name = "localized_utterance") val localizedUtterance: LocalesWithText?,
+    @ColumnInfo(name = "sort_order") val sortOrder: Int,
+    @Ignore
+    @ColumnInfo(name = "utterance_string_res") val utteranceStringRes: Int? = null,
+) : Parcelable {
+
+    // TODO: Remove this when we un-ignore [utteranceStringRes]. This constructor is needed for
+    //       Room to be able to instantiate the class b/c it doesn't know about utteranceStringRes
+    constructor(
+        phraseId: Long,
+        parentCategoryId: String?,
+        creationDate: Long,
+        lastSpokenDate: Long?,
+        localizedUtterance: LocalesWithText?,
+        sortOrder: Int,
+    ) : this(
+        phraseId = phraseId,
+        parentCategoryId = parentCategoryId,
+        creationDate = creationDate,
+        lastSpokenDate = lastSpokenDate,
+        localizedUtterance = localizedUtterance,
+        sortOrder = sortOrder,
+        utteranceStringRes = null,
+    )
+}
