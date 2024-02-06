6e47baa4efe5feb38dba4c3e85547fbc0e9f5308
nickreeve <nick.reeve@willowtreeapps.com>
2024-02-06 12:00:51 -0500

    Adds category field to CategoryButton & refactors button action/display logic around new field

--- app/src/main/java/com/willowtree/vocable/presets/CategoriesFragment.kt
+++ app/src/main/java/com/willowtree/vocable/presets/CategoriesFragment.kt
@@ -8,7 +8,6 @@ import android.widget.LinearLayout
 import androidx.core.view.children
 import androidx.core.view.isInvisible
 import androidx.core.view.isVisible
-import androidx.lifecycle.Observer
 import com.willowtree.vocable.BaseFragment
 import com.willowtree.vocable.BindingInflater
 import com.willowtree.vocable.R
@@ -16,8 +15,6 @@ import com.willowtree.vocable.customviews.CategoryButton
 import com.willowtree.vocable.customviews.PointerListener
 import com.willowtree.vocable.databinding.CategoriesFragmentBinding
 import com.willowtree.vocable.databinding.CategoryButtonBinding
-import com.willowtree.vocable.utils.locale.LocalizedResourceUtility
-import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ViewModelOwner
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -35,14 +32,14 @@ class CategoriesFragment : BaseFragment<CategoriesFragmentBinding>() {
         }
     }
 
-    override val bindingInflater: BindingInflater<CategoriesFragmentBinding> = CategoriesFragmentBinding::inflate
+    override val bindingInflater: BindingInflater<CategoriesFragmentBinding> =
+        CategoriesFragmentBinding::inflate
 
     private val viewModel: PresetsViewModel by viewModel(owner = {
         ViewModelOwner.from(requireActivity())
     })
     private val allViews = mutableListOf<View>()
     private var maxCategories = 1
-    private val localizedResourceUtility: LocalizedResourceUtility by inject()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -58,19 +55,16 @@ class CategoriesFragment : BaseFragment<CategoriesFragmentBinding>() {
             val categoryButton =
                 CategoryButtonBinding.inflate(inflater, binding.categoryButtonContainer, false)
             with(categoryButton.root) {
-                tag = category
-                action = {
-                    viewModel.onCategorySelected(category.categoryId)
-
-                }
-                text = localizedResourceUtility.getTextFromCategory(category)
+                setUpDisplayAndAction(
+                    category = category
+                ) { viewModel::onCategorySelected }
                 if (!isTablet && index > 0 && index + 1 == maxCategories) {
                     layoutParams = (layoutParams as LinearLayout.LayoutParams).apply {
                         marginStart = 0
                     }
                 }
+                binding.categoryButtonContainer.addView(this)
             }
-            binding.categoryButtonContainer.addView(categoryButton.root)
         }
         categories?.let {
             for (i in 0 until maxCategories - it.size) {
@@ -109,15 +103,15 @@ class CategoriesFragment : BaseFragment<CategoriesFragmentBinding>() {
     }
 
     private fun subscribeToViewModel() {
-        viewModel.selectedCategoryLiveData.observe(viewLifecycleOwner, Observer { category ->
+        viewModel.selectedCategoryLiveData.observe(viewLifecycleOwner) { category ->
             category?.let {
                 binding.categoryButtonContainer.children.forEach {
                     if (it is CategoryButton) {
-                        it.isSelected = (it.tag as Category).categoryId == category.categoryId
+                        it.isSelected = it.category?.categoryId == category.categoryId
                     }
                 }
             }
-        })
+        }
     }
 
     override fun getAllViews(): List<View> {
