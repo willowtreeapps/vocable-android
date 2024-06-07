f8efd1c0b4c0a8461aca6db28d192cc0eab920dc
Cindy Cui <misscindycui@gmail.com>
2024-06-07 15:12:28 -0700

    only scrolls to new category after immediately adding it, adds ViewModel tests

--- app/src/androidTest/java/com/willowtree/vocable/settings/EditCategoriesViewModelTest.kt
+++ app/src/androidTest/java/com/willowtree/vocable/settings/EditCategoriesViewModelTest.kt
@@ -1,5 +1,7 @@
 package com.willowtree.vocable.settings
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import app.cash.turbine.test
@@ -17,7 +19,14 @@ import com.willowtree.vocable.room.RoomStoredPhrasesRepository
 import com.willowtree.vocable.room.VocableDatabase
 import com.willowtree.vocable.utility.FakeDateProvider
 import com.willowtree.vocable.utility.StubLegacyCategoriesAndPhrasesRepository
+import com.willowtree.vocable.utility.VocableKoinTestRule
+import junit.framework.TestCase
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.advanceUntilIdle
 import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
@@ -25,7 +34,13 @@ import org.junit.Test
 class EditCategoriesViewModelTest {
 
     @get:Rule
+    val koinTestRule = VocableKoinTestRule()
+
+    @get:Rule
     val mainDispatcherRule = MainDispatcherRule()
+
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
 
     private val database = Room.inMemoryDatabaseBuilder(
         ApplicationProvider.getApplicationContext(),
@@ -68,6 +83,107 @@ class EditCategoriesViewModelTest {
         return EditCategoriesViewModel(
             categoriesUseCase
         )
+    }
+
+    // 1. refreshing categories without adding new category remains at first index
+    // 2. adding new category and refreshing once flips to new category's index
+    // 3. adding new category and refreshing twice flips to first index
+    // 4. adding new category amongst hidden categories and refreshing once flips to new category's index (before hidden categories)
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun  refreshing_categories_without_adding_new_category_remains_at_first_index() = runTest(UnconfinedTestDispatcher()) {
+        val vm = createViewModel()
+        vm.refreshCategories()
+
+        var index: Int? = null
+        val observer = Observer<Int> { value ->
+            index = value
+        }
+
+        try {
+            vm.lastViewedIndex.observeForever(observer)
+            advanceUntilIdle()
+            assertEquals(0, index)
+        } finally {
+            vm.lastViewedIndex.removeObserver(observer)
+        }
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun  adding_new_category_and_refreshing_once_flips_to_new_categorys_index() = runTest(UnconfinedTestDispatcher()) {
+        val vm = createViewModel()
+        vm.refreshCategories()
+
+        categoriesUseCase.addCategory("new category")
+        vm.refreshCategories()
+
+        var index: Int? = null
+        val observer = Observer<Int> { value ->
+            index = value
+        }
+
+        try {
+            vm.lastViewedIndex.observeForever(observer)
+            advanceUntilIdle()
+            assertEquals(7, index)
+        } finally {
+            vm.lastViewedIndex.removeObserver(observer)
+        }
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun  adding_new_category_and_refreshing_twice_flips_to_first_index() = runTest(UnconfinedTestDispatcher()) {
+        val vm = createViewModel()
+        vm.refreshCategories()
+
+        categoriesUseCase.addCategory("new category")
+        vm.refreshCategories()
+        vm.refreshCategories()
+
+        var index: Int? = null
+        val observer = Observer<Int> { value ->
+            index = value
+        }
+
+        try {
+            vm.lastViewedIndex.observeForever(observer)
+            advanceUntilIdle()
+            assertEquals(0, index)
+        } finally {
+            vm.lastViewedIndex.removeObserver(observer)
+        }
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun  adding_new_category_amongst_hidden_categories_and_refreshing_once_flips_to_last_non_hidden_index() = runTest(UnconfinedTestDispatcher()) {
+        val vm = createViewModel()
+        categoriesUseCase.updateCategoryHidden("preset_general", hidden = true)
+        categoriesUseCase.updateCategoryHidden("preset_basic_needs", hidden = true)
+
+        vm.refreshCategories()
+
+        categoriesUseCase.addCategory("new category")
+
+        categoriesUseCase.categories().first()
+
+        vm.refreshCategories()
+
+        var index: Int? = null
+        val observer = Observer<Int> { value ->
+            index = value
+        }
+
+        try {
+            vm.lastViewedIndex.observeForever(observer)
+            advanceUntilIdle()
+            assertEquals(5, index)
+        } finally {
+            vm.lastViewedIndex.removeObserver(observer)
+        }
     }
 
     @Test
